f8da4d17bc38ca6c8166d5c952755e6b1260f47b
eastddh <362693729@qq.com>
2018-03-11 00:33:42 +0800

    Update closeSerial.m

--- closeSerial.m
+++ closeSerial.m
@@ -1,3 +1,6 @@
+% a typical way to close the serial
+% Sometimes Matlab doesn't allow to connect the same arduino as two object.
+% To eliminate the error
 if ~isempty(instrfind)
     fclose(instrfind);
     delete(instrfind);
